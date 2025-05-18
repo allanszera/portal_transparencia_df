1227989fb0f9b4d9cc4bd07b5447f0be7bf6563c
allanszera <allandifere@gmail.com>
2025-05-18 11:17:17 -0300

    Tratamento dos dados concluído

---
+++ src/transform.py
@@ -0,0 +1,59 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+
+load_dotenv()
+
+parquet_files = os.getenv("PARQUETS").split(',')
+
+dfs = [pd.read_parquet(path) for path in parquet_files]
+df = pd.concat(dfs, ignore_index=True)
+
+df['LÍQUIDO'] = df['LÍQUIDO'].str.replace(',', '.', regex=False)
+df['LÍQUIDO'] = pd.to_numeric(df['LÍQUIDO'], errors='coerce')
+df['ANO'] = df['ANO'].astype(int)
+df['MÊS'] = df['MÊS'].astype(int)
+
+# Lista das colunas que precisam ser transformadas para float
+colunas_valores = [
+    "REMUNERAÇÃO BÁSICA",
+    "BENEFÍCIOS",
+    "VALOR DA FUNÇÃO",
+    "COMISSÃO CONSELHEIRO",
+    "HORA EXTRA",
+    "VERBAS EVENTUAIS",
+    "VERBAS JUDICIAIS",
+    "DESCONTOS A MAIOR",
+    "LICENÇA PRÊMIO",
+    "IRRF",
+    "SEG. SOCIAL",
+    "TETO REDUTOR",
+    "OUTROS RECEBIMENTOS",
+    "OUTROS DESCONTOS OBRIGATÓRIOS",
+    "PAGAMENTOS A MAIOR",
+    "BRUTO"
+]
+
+for col in colunas_valores:
+    df[col] = (
+        df[col].astype(str)
+        .str.replace('.', '', regex=False)      # Remove pontos de milhar (se houver)
+        .str.replace(',', '.', regex=False)     # Troca vírgula decimal por ponto
+        .str.replace('R$', '', regex=False)     # Remove símbolo R$ se houver
+        .str.strip()                            # Remove espaços
+    )
+    df[col] = pd.to_numeric(df[col], errors='coerce')
+
+
+
+print(df.dtypes)
+print(df[['MÊS','ANO']].drop_duplicates())
+print(df['LÍQUIDO'])
+
+
+# Após tratamento dos valores numéricos...
+
+# Salve o DataFrame tratado em um único arquivo Parquet
+df.to_parquet("data/servidores_tratado.parquet", index=False)
+
+print("Arquivo data/servidores_tratado.parquet salvo com sucesso!")
